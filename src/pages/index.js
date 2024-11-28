cc901b4f06ff44ab9888c921321723a2cf0ad3ee
pileuszu <pileuszu@gmail.com>
2024-11-28 22:21:32 +0900

    background color changed

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+// Main 페이지 export
+export { default as Main } from './main';
+
+// 나중에 다른 페이지들이 추가될 때마다 여기서 export
+// export { default as About } from './about';
+// export { default as Contact } from './contact';
+// export { default as Blog } from './blog';
---
+++ src/pages/main/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Main.css';
+
+function Main() {
+  return (
+    <div className="main-background">
+    </div>
+  );
+}
+
+export default Main;
